257fdcc72bcf4aff0adcccae479541c438d5f99e
muzzamilbukhari <muhammadmuzzamilbukhari@gmail.com>
2025-03-09 17:50:16 +0500

    fix

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ from utils.utils import save_sample_image
 from torchvision import transforms
 
 def main():
-    checkpoint_path = "/content/drive/MyDrive/ldr2hdr/checkpoints/checkpoint_epoch_100.pth"
+    checkpoint_path = "/content/drive/MyDrive/AI_Project_LDR2HDR/checkpoints/checkpoint_epoch_100.pth"
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     
     model = UNet(n_channels=3, n_classes=3, bilinear=True).to(device)
@@ -17,16 +17,16 @@ def main():
     
     transform = transforms.Compose([transforms.ToTensor()])
     
-    test_dataset = LDRHDRDataset(root_dir="/content/drive/MyDrive/ldr2hdr/data/val",
+    test_dataset = LDRHDRDataset(root_dir="/content/drive/MyDrive/AI_Project_LDR2HDR/data/val",
                                  ldr_dir="LDR", hdr_dir="HDR", transform=transform)
     test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
     
-    os.makedirs("/content/drive/MyDrive/ldr2hdr/results", exist_ok=True)
+    os.makedirs("/content/drive/MyDrive/AI_Project_LDR2HDR/results", exist_ok=True)
     with torch.no_grad():
         for idx, (ldr, _) in enumerate(test_loader):
             ldr = ldr.to(device)
             output = model(ldr)
-            save_sample_image(output, filename=f"/content/drive/MyDrive/ldr2hdr/results/output_{idx}.png")
+            save_sample_image(output, filename=f"/content/drive/MyDrive/AI_Project_LDR2HDR/results/output_{idx}.png")
     
 if __name__ == "__main__":
     main()
